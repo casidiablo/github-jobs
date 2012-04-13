dbf00b6e583dc42b7dd683ce6923c7f38b4a6971
Cristian <casidiablo@elhacker.net>
2012-04-13 15:09:19 -0500

    - Specify that job table is not autoincrement

--- src/com/github/jobs/App.java
+++ src/com/github/jobs/App.java
@@ -10,7 +10,6 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         // configure database
-        SqlPersistence database = PersistenceConfig.getDatabase(getPackageName(), 1);
-        database.match(Job.class);
+        PersistenceConfig.getDatabase(getPackageName(), 1).matchNotAutoIncrement(Job.class);
     }
 }
