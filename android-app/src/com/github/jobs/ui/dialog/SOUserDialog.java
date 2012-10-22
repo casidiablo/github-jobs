7085f46ae470706b659c8ed17cd1c1e716250da4
Margge <marcelaguiza@gmail.com>
2012-10-22 16:33:39 -0500

    Added new features on the user dialog.

--- android-app/src/com/github/jobs/ui/dialog/SOUserDialog.java
+++ android-app/src/com/github/jobs/ui/dialog/SOUserDialog.java
@@ -1,8 +1,12 @@
 package com.github.jobs.ui.dialog;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.util.Linkify;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,11 +23,12 @@ import com.telly.wasp.BitmapUtils;
 public class SOUserDialog extends TrackDialog {
 
     public static final String EXTRA_USER = "com.github.jobs.extra.user";
+    private String mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SOUser soUser = (SOUser) getIntent().getParcelableExtra(EXTRA_USER);
+        final SOUser soUser = (SOUser) getIntent().getParcelableExtra(EXTRA_USER);
         if (soUser == null) {
             Toast.makeText(this, R.string.invalid_so_user, Toast.LENGTH_LONG).show();
             finish();
@@ -31,12 +36,35 @@ public class SOUserDialog extends TrackDialog {
         }
         setContentView(R.layout.so_user_dialog);
 
-        TextView displayName = (TextView) findViewById(R.id.lbl_username);
-        displayName.setText(soUser.getDisplayName());
+        mTitle = soUser.getDisplayName();
 
         TextView reputation = (TextView) findViewById(R.id.lbl_reputation);
-        reputation.setText(getString(R.string.reputation, soUser.getDisplayName()));
+        reputation.setText(getString(R.string.reputation, soUser.getReputation()));
 
+        TextView website = (TextView) findViewById(R.id.lbl_website);
+        website.setText(getString(R.string.website, soUser.getWebsiteUrl()));
+
+        TextView goldBadge = (TextView) findViewById(R.id.lbl_gold);
+        TextView silverBadge = (TextView) findViewById(R.id.lbl_silver);
+        TextView bronzeBadge = (TextView) findViewById(R.id.lbl_bronze);
+
+        int gold = soUser.getBadgeCount().getGold();
+        int silver = soUser.getBadgeCount().getSilver();
+        int bronze = soUser.getBadgeCount().getBronze();
+        if (gold > 0) {
+            goldBadge.setVisibility(View.VISIBLE);
+            goldBadge.setText(" " + String.valueOf(gold));
+        }
+        if (silver > 0) {
+            silverBadge.setVisibility(View.VISIBLE);
+            silverBadge.setText(" " + String.valueOf(silver));
+        }
+        if (bronze > 0) {
+            bronzeBadge.setVisibility(View.VISIBLE);
+            bronzeBadge.setText(" " + String.valueOf(bronze));
+        }
+
+        Linkify.addLinks(website, Linkify.WEB_URLS);
 
         ImageView userAvatar = (ImageView) findViewById(R.id.img_user_avatar);
         BitmapHelper bitmapHelper = BitmapHelper.getInstance();
@@ -48,5 +76,27 @@ public class SOUserDialog extends TrackDialog {
             BitmapObserver observer = new BitmapObserver(userAvatar, soUser.getProfileImage(), new Handler());
             bitmapHelper.registerBitmapObserver(this, observer);
         }
+        userAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchWebsite(soUser.getLink());
+            }
+        });
+    }
+
+    private void launchWebsite(String url) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(this, R.string.could_not_launch_url, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onTitleChanged(CharSequence title, int color) {
+        super.onTitleChanged(this.mTitle, color);
     }
 }
