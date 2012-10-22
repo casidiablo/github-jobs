868e720550cec29daf07624bfeca36c09c5e57c7
Cristian <cristian@elhacker.net>
2012-10-21 23:05:49 -0500

    Added skeleton for Login Activity

---
+++ android-app/src/com/github/jobs/utils/TextWatcherAdapter.java
@@ -0,0 +1,19 @@
+package com.github.jobs.utils;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * @author cristian
+ */
+public class TextWatcherAdapter implements TextWatcher {
+
+    public void afterTextChanged(Editable s) {
+    }
+
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+}
